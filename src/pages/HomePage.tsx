ade05cec1260a266c3e556991c23d2ec384ee520
Marcos Benitez <122688458+Marcos-Bntz@users.noreply.github.com>
2025-07-08 09:43:26 -0300

    Crear carrusel de florerías

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Hero } from '../components/home/Hero';
-import { FeaturedProducts } from '../components/home/FeaturedProducts';
+import { FloreriaCarousel } from '../components/home/FloreriaCarousel';
 import { Categories } from '../components/home/Categories';
 import { Testimonials } from '../components/home/Testimonials';
 import { CTA } from '../components/home/CTA';
-import { products } from '../data/products';
 
 export const HomePage: React.FC = () => {
   return (
     <div>
       <Hero />
-      <FeaturedProducts products={products} />
+      <FloreriaCarousel />
       <Categories />
       <Testimonials />
       <CTA />
